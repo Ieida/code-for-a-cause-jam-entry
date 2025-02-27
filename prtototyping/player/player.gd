129f084c848f77e52b462c972792226a796cdebb
ArkKive <Campbell.morris77@gmail.com>
2025-02-27 18:54:45 +1300

    Changes with coyote
    Changes with coyote jump, and ground buffer.
    
    player can still jump if they press it slightly before hitting ground. Player can still briefly jump even after walking off an edge.

--- prtototyping/player/player.gd
+++ prtototyping/player/player.gd
@@ -7,10 +7,15 @@ extends CharacterBody2D
 @export var gravity: float = 981
 @export var speed: float = 128
 @export var jump_velocity = -300
+@export var default_coyote_time: float = 0.5
+@export var default_ground_coyote_time: float = 0.5
 var apply_gravity: bool = true
 var can_move: bool = true
 var is_falling: bool
-
+var can_jump: bool
+#coyote time var
+var coyote_time: float
+var ground_coyote_time: float
 
 func _on_anim_finished():
 	if animated_sprite.animation == &"float":
@@ -26,8 +31,24 @@ func _physics_process(delta: float) -> void:
 	if apply_gravity and not is_on_floor():
 		velocity.y += gravity * delta
 
+	# Is on ground buffer.
+	if ground_coyote_time > 0:
+		can_jump = true
+	else:
+		can_jump = false
+	# If jump, apply coyote.
+	if Input.is_action_just_pressed(&"jump"):
+		coyote_time = default_coyote_time
+	# Deduct coyote.
+	if coyote_time > 0:
+		coyote_time -= delta
+	else:
+		coyote_time = 0
+	
 	# Handle jump.
-	if can_move and Input.is_action_just_pressed(&"jump") and is_on_floor():
+	if can_move and coyote_time > 0 and can_jump:
+		ground_coyote_time = 0
+		coyote_time = 0
 		velocity.y = jump_velocity
 	
 	var direction := Input.get_axis(&"move_left", &"move_right")
@@ -53,6 +74,8 @@ func _physics_process(delta: float) -> void:
 		if not is_zero_approx(direction):
 			animated_sprite.flip_h = velocity.x < 0.0
 		if is_on_floor():
+			# If grounded, set ground coyote.
+			ground_coyote_time = default_ground_coyote_time
 			if velocity.x > 2.0:
 				animated_sprite.play(&"run")
 			elif velocity.x < -2.0:
@@ -61,6 +84,9 @@ func _physics_process(delta: float) -> void:
 				animated_sprite.play(&"idle")
 		elif velocity.y < -2.0:
 			animated_sprite.play(&"ascend")
+		# Of not grounded, deduct coyote (jump still allowed)
+		elif ground_coyote_time > 0:
+			ground_coyote_time -= delta
 		
 		if is_falling and velocity.y < 2.0:
 			can_move = false
